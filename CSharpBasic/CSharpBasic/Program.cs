12357b2ffc927d48a658461588118a4c0d1af088
andmar99 <ndrmaria79@gmail.com>
2024-01-19 11:53:16 +0200

    commit

--- CSharpBasic/CSharpBasic/Program.cs
+++ CSharpBasic/CSharpBasic/Program.cs
@@ -139,7 +139,6 @@ namespace CSharpBasic
                 Console.WriteLine("{0}", mathNotes[i]);
             }
 
-
             var students = new List<string>() {"Mihai", "Cristina" };
             //Console.WriteLine(students); //incorect
             Console.WriteLine("Students list: ");
